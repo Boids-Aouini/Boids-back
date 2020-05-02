bd7ae5b82cacf4a3eb70fd2c471513325c8985c2
aminaouini <aminaouini565@gmail.com>
2020-05-02 03:16:48 +0100

    added doc

--- db/connectToDB/connectToDB.js
+++ db/connectToDB/connectToDB.js
@@ -1,15 +1,15 @@
 let mysql = require('mysql');
 
-var db = mysql.createConnection({
+var connection = mysql.createConnection({ // make connection and provide db information on mysql server
     host: "localhost",
     user: "root",
     password: "123456789",
     database: 'Boids'
 });
 
-db.connect(function (err) {
-    if (err) throw err;
-    console.log("DB Connected!");
+connection.connect(function (err) { // connect to db
+    if (err) throw err; // throw error in case there is one
+    console.log("DB Connected!"); // run this line in case every thing went well 
 });
 
-module.exports = db;
+module.exports = connection; // export connected mysql server
