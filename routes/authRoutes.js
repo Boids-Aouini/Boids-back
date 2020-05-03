7dd6413a8818d5dc8070cab217781236fb928b47
aminaouini <aminaouini565@gmail.com>
2020-05-04 00:03:35 +0100

    made cipher pass

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,7 +1,13 @@
-
+let CryptoJS = require('crypto-js');
 
 module.exports = (app, Con) => {
     app.post('/register', async (req, res) => {
+        let { firstname, lastname, email,
+            password, longitude, latitude,
+            heighAccuracy, createdAt } = req.body;
+
+        const cipherPass = CryptoJS.AES.encrypt(password, process.env.PASSWORD_SECRET_KEY);
+
 
     })
 }
