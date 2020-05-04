d7a0e644fb64522ec199088460e454fa8aaaffcf
aminaouini <aminaouini565@gmail.com>
2020-05-04 02:09:12 +0100

    setup new account insert

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,13 +1,24 @@
 let CryptoJS = require('crypto-js');
 
 module.exports = (app, Con) => {
-    app.post('/register', async (req, res) => {
+    app.post('/register', async (req, response) => {
         let { firstname, lastname, email,
             password, longitude, latitude,
-            heighAccuracy, createdAt } = req.body;
+            heighAccuracy, createdAt, birthDate } = req.body;
+        const cipherPass = CryptoJS.AES.encrypt(password, process.env.PASSWORD_SECRET_KEY).toString();
 
-        const cipherPass = CryptoJS.AES.encrypt(password, process.env.PASSWORD_SECRET_KEY);
+        let newAcc = [firstname, lastname, email,
+            cipherPass, birthDate, longitude, latitude,
+            heighAccuracy, createdAt];
 
+        await Con.query(`INSERT INTO Users (firstname, lastname, email, password, birthDate, longitude, latitude, heighAccuracy, createdAt) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`, newAcc,
+            (err, result, fields) => {
+                if (err) { response.staus(401).send('email is already used') }
+                else {
 
+                    console.log(result.insertId)
+                }
+
+            })
     })
 }
