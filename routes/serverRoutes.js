d79dce68313c234047526a5718c7aafc525071ee
aminaouini <aminaouini565@gmail.com>
2020-05-06 04:54:20 +0100

    added query to create server route

--- routes/serverRoutes.js
+++ routes/serverRoutes.js
@@ -2,7 +2,20 @@ let verify = require('./verifyUser');
 
 module.exports = (app, Con) => {
     app.post('/createServer', verify, (req, res) => {
-        let { id } = req.user;
+        let user_id = req.user.id;
         let { name, createdAt } = req.body;
+        console.log(req.user)
+        let newServer = [user_id, name, createdAt];
+
+        Con.query('INSERT INTO Servers (user_id, name, createdAt) VALUES (?, ?, ?)', newServer,
+            (err, results) => {
+                if (err) { res.status(400).send('Server\'s name is used already').end() }
+                res.send({
+                    results: {
+                        response: "Handeled create new server request",
+                        server_id: results.insertId
+                    }
+                })
+            })
     })
 }
