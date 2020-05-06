889d51f3fd4814e449b9f045a30ea60546695127
aminaouini <aminaouini565@gmail.com>
2020-05-06 03:48:04 +0100

    added doc

--- routes/verifyUser.js
+++ routes/verifyUser.js
@@ -3,12 +3,12 @@ let jwt = require('jsonwebtoken');
 module.exports = function (req, res, next) {
     let token = req.header('auth_token');
 
-    if (!token) { res.status(401).send('access denied').end() }
+    if (!token) { res.status(401).send('access denied').end() } // send error if there is not token
     try {
-        let verifiedUser = jwt.verify(token, process.env.TOKEN_SECRET_KEY);
-        req.user = verifiedUser;
-        next();
+        let verifiedUser = jwt.verify(token, process.env.TOKEN_SECRET_KEY); // verify token
+        req.user = verifiedUser; // set req.user to verified user 
+        next(); // run the next middleware
     } catch (err) {
-        res.status(400).send('Invalid Token !')
+        res.status(400).send('Invalid Token !') // send error if token is invalid
     }
 }
