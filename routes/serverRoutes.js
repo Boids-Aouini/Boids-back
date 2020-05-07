0ae99391879b9ad9c384705dde62e339d10bf6f0
aminaouini <aminaouini565@gmail.com>
2020-05-07 07:26:56 +0100

    added create new channel when ever make new server

--- routes/serverRoutes.js
+++ routes/serverRoutes.js
@@ -8,15 +8,19 @@ module.exports = (app, Con) => {
         let newServer = [user_id, name, createdAt]; // set new server data for query
 
         Con.query('INSERT INTO Servers (leader_id, name, createdAt) VALUES (?, ?, ?)', newServer, // make insertion query
-            (err, result) => { // callback once done with query
+            (err, ServerResult) => { // callback once done with query
                 if (err) { res.status(400).send('Server\'s name is used already').end() } // send error in case there is one
+                Con.query('INSERT INTO Channels (server_id, name, createdAt) VALUES', [ServerResult.insertId, 'Announcement', createdAt],
 
-                res.send({ // send successful response
-                    results: {
-                        response: "Handeled create new server request",
-                        server: result.insertId
-                    }
-                })
+                    (err, resultChannel) => {
+                        if (err) { res.status(400).send(err).end() }
+                        res.send({ // send successful response
+                            results: {
+                                response: "Handeled create new server request",
+                                server: result.insertId
+                            }
+                        })
+                    })
             })
     })
 }
