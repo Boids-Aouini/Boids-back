5930b84a1ab3a13bcb64ebaa3d4ad09cded1f4e0
aminaouini <aminaouini565@gmail.com>
2020-05-08 03:05:53 +0100

    modefied auth routes to use router instead of app

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,59 +1,62 @@
 let CryptoJS = require('crypto-js');
 let jwt = require('jsonwebtoken');
+let express = require('express');
+let router = express.Router();
+let Con = require('../db/connectToDB/connectToDB')
 
-module.exports = (app, Con) => {
-    app.post('/register', async (req, response) => {
-        let { firstname, lastname, email,
-            password, createdAt, birthDate } = req.body;
-        const cipherPass = CryptoJS.AES.encrypt(password, process.env.PASSWORD_SECRET_KEY).toString(); // crypte password
+router.post('/register', async (req, response) => {
+    let { firstname, lastname, email,
+        password, createdAt, birthDate } = req.body;
+    const cipherPass = CryptoJS.AES.encrypt(password, process.env.PASSWORD_SECRET_KEY).toString(); // crypte password
 
-        let newAcc = [firstname, lastname, email,
-            cipherPass, birthDate, createdAt]; // new account data that is going to be stored in the db
+    let newAcc = [firstname, lastname, email,
+        cipherPass, birthDate, createdAt]; // new account data that is going to be stored in the db
 
-        await Con.query(`INSERT INTO Users (firstname, lastname, email, password, birthDate, createdAt) VALUES (?, ?, ?, ?, ?, ?)`, newAcc,
-            (err, result) => {
-                if (err) { response.status(401).send('email is already used') } // send err in case there is one
-                else {
+    await Con.query(`INSERT INTO Users (firstname, lastname, email, password, birthDate, createdAt) VALUES (?, ?, ?, ?, ?, ?)`, newAcc,
+        (err, result) => {
+            if (err) { response.status(401).send('email is already used') } // send err in case there is one
+            else {
 
 
-                    let token = jwt.sign({ id: result.insertId }, process.env.TOKEN_SECRET_KEY); // make token
-                    response.status(201).send({ // send successful response with token
+                let token = jwt.sign({ id: result.insertId }, process.env.TOKEN_SECRET_KEY); // make token
+                response.status(201).send({ // send successful response with token
+                    results: {
+                        token
+                    }
+                })
+            }
+
+        })
+})
+
+router.post('/login', async (req, response) => {
+    let { email, password } = req.body;
+    console.log(req.body)
+    await Con.query("SELECT id, password FROM Users WHERE email = (?)", email, (err, result) => {
+        if (err) { response.status(400).send(err).end() } // send error in case there is one
+        else {
+            // console.log(!!result[0], result)
+            if (!!result[0]) {
+                let encryptedPass = result[0].password;
+                let bytes = CryptoJS.AES.decrypt(encryptedPass, process.env.PASSWORD_SECRET_KEY);
+                let decryptedPass = JSON.parse(bytes.toString(CryptoJS.enc.Utf8)); // decrypt password
+                if (password + "" === decryptedPass + "") {
+                    let token = jwt.sign({ id: result[0].id }, process.env.TOKEN_SECRET_KEY); // make token if creds is valid
+                    response.status(201).send({ // send response with the token
                         results: {
                             token
                         }
                     })
-                }
-
-            })
-    })
-
-    app.post('/login', async (req, response) => {
-        let { email, password } = req.body;
-        console.log(req.body)
-        await Con.query("SELECT id, password FROM Users WHERE email = (?)", email, (err, result) => {
-            if (err) { response.status(400).send(err).end() } // send error in case there is one
-            else {
-                // console.log(!!result[0], result)
-                if (!!result[0]) {
-                    let encryptedPass = result[0].password;
-                    let bytes = CryptoJS.AES.decrypt(encryptedPass, process.env.PASSWORD_SECRET_KEY);
-                    let decryptedPass = JSON.parse(bytes.toString(CryptoJS.enc.Utf8)); // decrypt password
-                    if (password + "" === decryptedPass + "") {
-                        let token = jwt.sign({ id: result[0].id }, process.env.TOKEN_SECRET_KEY); // make token if creds is valid
-                        response.status(201).send({ // send response with the token
-                            results: {
-                                token
-                            }
-                        })
-                    } else {
-                        response.status(401).send('invalid password') // send invalid token in case that it is
-                    }
-
                 } else {
-                    response.status(401).send('email is not found') // send email not found in case that it is
+                    response.status(401).send('invalid password') // send invalid token in case that it is
                 }
-            }
-        })
 
+            } else {
+                response.status(401).send('email is not found') // send email not found in case that it is
+            }
+        }
     })
-}
+
+})
+
+module.exports = router;
--- routes/authRoutes.test.js
+++ routes/authRoutes.test.js
@@ -1,17 +1,22 @@
 let app = require('../index');
 let request = require('supertest');
+describe('authentication routes testing', () => {
 
-test('should retreive token once register', function () {
-    const response = request(app);
-    response.post('/register')
-        .send({
-            firstname: 'nameTest',
-            lastname: 'lastnameTest',
-            email: 'test@test.com',
-            password: '123456789',
-            birthDate: '2000-12-12',
-            createdAt: '2020-05-05'
-        })
-    console.log(response)
+    it('should retreive token once register', async () => {
+        const res = await request(app)
+            .post('/register')
+            .send({
+                firstname: 'nameTest',
+                lastname: 'lastnameTest',
+                email: 'test@test.com',
+                password: '123456789',
+                birthDate: '2000-12-12',
+                createdAt: '2020-05-05'
+            })
+
+        console.log(res.body)
+        expect(res.statusCode).toBeEqual(201)
+
+    })
 
 })
