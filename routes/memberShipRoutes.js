7762bb69ea3412cfde09a7acedc54ed6f5b4faf3
aminaouini <aminaouini565@gmail.com>
2020-05-11 01:29:00 +0100

    retreived leader id from server by it's name

--- routes/memberShipRoutes.js
+++ routes/memberShipRoutes.js
@@ -1,8 +1,14 @@
 let { Router } = require('express');
 let router = Router();
 let verify = require('./verifyUser');
+let Con = require('../db/connectToDB/connectToDB');
 
 router.post('/createMembership', verify, (req, res) => {
+    let { serverName } = req.body
+    Con.query('SELECT leader_id FROM Servers WHERE name = (?)', [serverName], (err, result) => {
+        if (err) { return res.status(400).send(err).end() }
+
+    })
 
 })
 
