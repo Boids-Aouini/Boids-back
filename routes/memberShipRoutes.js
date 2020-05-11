384efccb711e19b435bcd8810f1c9124fc4675c1
aminaouini <aminaouini565@gmail.com>
2020-05-11 03:27:26 +0100

    only leader can add new members

--- routes/memberShipRoutes.js
+++ routes/memberShipRoutes.js
@@ -9,6 +9,8 @@ router.post('/createMembership', verify, (req, res) => {
     Con.query('SELECT leader_id FROM Servers where id = (?)', [server_id], (err, result) => {
         if (err) { return res.status(400).send(err).end() }
         if (!result[0]) { return res.status(404).send('server is not found').end() }
+        let { leader_id } = result[0];
+        if (leader_id !== res.user.id) { return res.status(400).send('only leader can add new members') }
     })
 
     Con.query('SELECT firstname, lastname FROM Users WHERE email = (?)', [newMemberEmail], (err, result) => {
