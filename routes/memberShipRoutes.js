b21b22b485e989a971efe869fbbab77910b452ee
aminaouini <aminaouini565@gmail.com>
2020-05-11 02:58:02 +0100

    setup transporter in create membership route

--- routes/memberShipRoutes.js
+++ routes/memberShipRoutes.js
@@ -9,8 +9,17 @@ router.post('/createMembership', verify, (req, res) => {
     Con.query('SELECT name FROM Users WHERE email = (?)', [newMemberEmail], (err, result) => {
         if (err) { return res.status(400).send(err).end() }
         if (!result[0]) { return res.status(404).send('users with this email doesn\'t have an account unfortunately').end() }
+        let { name } = result[0];
 
     })
+
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.BOIDS_EMAIL,
+            pass: process.env.BOIDS_PASS
+        }
+    });
 
 
 })
