450d329a25022fca750ec39fe31084c3e6ae86bc
aminaouini <aminaouini565@gmail.com>
2020-05-12 09:36:51 +0100

    fixed a prob

--- routes/memberShipRoutes.js
+++ routes/memberShipRoutes.js
@@ -6,64 +6,60 @@ router = Router(),
 
 router.post('/createMembership', verify, (req, res) => {
     let { server_id, email, message, role } = req.body;
-    console.log(req.user.id, email, server_id)
-    let leader_id,
-        firstname,
-        lastname,
-        name,
-        id;
+
     Con.query('SELECT Users.id, Users.firstname, Users.lastname, Servers.leader_id, Servers.name FROM Users INNER JOIN Servers ON Servers.leader_id = (?) AND Users.email = (?) AND Servers.id = (?)', [req.user.id, email, server_id], (err, result) => {
         // retreive data needed from users and servers tables
         if (err) { return res.status(400).send(err).end() } // send error in case there is one
         if (!result[0]) { return res.status(401).send('data is not found').end() } // send server not found in case it's not found
-        let leader = result[0].leader_id;
-        if (leader !== res.user.id) { return res.status(400).send('only leader can add new members') } // send only leader can add new members if sender is 
-        leader_id = result[0].leader_id,
-            firstname = result[0].firstname,
-            lastname = result[0].lastname,
-            name = result[0].name,
-            id = result[0].id;
+        let { leader_id,
+            firstname,
+            lastname,
+            name,
+            id } = result[0];
+
+        if (leader_id !== req.user.id) { return res.status(400).send('only leader can add new members') } // send only leader can add new members if sender is 
+        console.log(id, leader_id)
+        Con.query('INSERT INTO Servers_Memberships (server_id, user_id, role) VALUES (?, ?, ?)', [server_id, id, role],
+            // insert new membership in server_memeberships table    
+            (err, result) => {
+                if (err) { return res.status(400).send(err).end() }// send error in case there is one
+                console.log('making transprter')
+                const transporter = nodemailer.createTransport({ // make transporter
+                    service: 'gmail',
+                    auth: {
+                        user: process.env.BOIDS_MAIL,
+                        pass: process.env.BOIDS_PASS // naturally, replace both with your real credentials or an application-specific password
+                    },
+                    tls: { // run locally
+                        rejectUnauthorized: false
+                    }
+                });
+
+                const mailOptions = { // make mail options
+                    from: process.env.BOIDS_MAIL,
+                    to: email,
+                    subject: 'New ' + name + ' member in',
+                    html: `
+                        <p>${message}</p>
+                    `
+                };
+                console.log('sending email')
+                transporter.sendMail(mailOptions, (error, info) => { // send email
+                    if (error) {
+                        return res.status(400).send('Couldn\' send email to member').end();
+                    }
+
+                    res.status(201).send({ // send successful request
+                        results: {
+                            response: 'New member has been added'
+                        }
+                    }).end();
+                });
+
+
+            })
     })
 
-    Con.query('INSERT INTO Servers_Memberships (server_id, user_id, role) VALUES (?, ?, ?)', [server_id, id, role],
-        // insert new membership in server_memeberships table    
-        (err, result) => {
-            if (err) { return res.status(400).send(err).end() }// send error in case there is one
-
-            const transporter = nodemailer.createTransport({ // make transporter
-                service: 'gmail',
-                auth: {
-                    user: process.env.BOIDS_MAIL,
-                    pass: process.env.BOIDS_PASS // naturally, replace both with your real credentials or an application-specific password
-                },
-                tls: { // run locally
-                    rejectUnauthorized: false
-                }
-            });
-
-            const mailOptions = { // make mail options
-                from: process.env.BOIDS_MAIL,
-                to: email,
-                subject: 'New ' + name + ' member in',
-                html: `
-                <p>${message}</p>
-            `
-            };
-
-            transporter.sendMail(mailOptions, (error, info) => { // send email
-                if (error) {
-                    return res.status(400).send('Couldn\' send email to member').end();
-                }
-
-                res.status(201).send({ // send successful request
-                    results: {
-                        response: 'New member has been added'
-                    }
-                }).end();
-            });
-
-
-        })
 
 
 })
