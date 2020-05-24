397a31200dd532c16cfe7931123c76ef66fe1f46
aminaouini <aminaouini565@gmail.com>
2020-05-24 15:56:38 +0100

    console new client socket id once connected

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ let PORT = process.env.PORT = 4404;
 let server = http.createServer(app);
 let io = socketIO(server);
 io.on('connection', socket => {
-    console.log(socket);
+    console.log('New Client is connected', socket.id);
     socket.on('disconnect', () => {
         console.log(`Client disconnected ${socket.id}`)
     })
